a47b5e00784746a4f192f21ba1ebe5b3207e00b2
agusdelavega <delavega.agus@gmail.com>
2020-08-15 14:28:41 -0300

    ISSUE-000:toggle user list

--- client/src/components/user-list/UserList.tsx
+++ client/src/components/user-list/UserList.tsx
@@ -6,22 +6,60 @@ export type Props = {
   users?: User[],
 }
 
-export class UserList extends Component<Props> {
+type State = {
+    hidden: boolean
+}
+
+export class UserList extends Component<Props,State> {
+  constructor(props : Props) {
+    super(props);
+    this.state = {
+      hidden : false
+    }
+  }
+
+  handleChange() {
+    const {hidden} = this.state;
+    this.setState({hidden : !hidden})
+  }
+
   render() {
     const { users } = this.props
+    const { hidden } = this.state;
     const usersItems = users && users.map(user => {
       return (
         <li className="list-group-item">
-          <span>{user.name}</span>
+          <span>{user.name + " "}</span>
+          <span>{user.surname}</span>
         </li>
       )
     })
-    return (
-      <div className="container">
-        <h2>Lista de usuarios</h2>
-        <ul className="list-group user-list">
-          {usersItems}
-        </ul>
+
+    const toggle =
+    <div>
+      <h3>Mostrar lista de usuarios </h3>
+      <label className="switch">
+        <input type="checkbox" onChange={() => this.handleChange()} checked={!this.state.hidden}/>
+        <span className="slider round"/>
+      </label>
+    </div>;
+
+
+    if (hidden) {
+        return (
+            toggle
+        )
+      }
+
+      return (
+      <div>
+        {toggle}
+        <div className="user_container">
+          <h2>Lista de usuarios</h2>
+          <ul className="list-group user-list">
+            {usersItems}
+          </ul>
+        </div>
       </div>
     )
   }
