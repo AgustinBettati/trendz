c0a5eba08e7f78f0df2d0a52732c92138db6c933
sofiasidanez <sofia.sidanez@ing.austral.edu.ar>
2020-08-17 20:48:31 -0300

    ISSUE 001:UserList toggle now works!

--- client/src/components/user-list/UserList.tsx
+++ client/src/components/user-list/UserList.tsx
@@ -1,30 +1,67 @@
 import React, { Component } from 'react'
 import { User } from '../types/types'
 import "./UserList.css"
+import {getAllUsers} from "../../api/UserApi";
 
 export type Props = {
   users?: User[],
+
 }
 
-export class UserList extends Component<Props> {
-  render() {
-    const { users } = this.props
-    const usersItems = users && users.map(user => {
-      return (
-        <li className="list-group-item">
-          <span>{user.name}</span>
-        </li>
-      )
-    })
-    return (
-      <div className="container">
-        <h2>Lista de usuarios</h2>
-        <ul className="list-group user-list">
-          {usersItems}
-        </ul>
-      </div>
-    )
-  }
+type State = {
+    showList: boolean
+
 }
+
+
+export class UserList extends Component<Props, State>{
+    constructor(props: Props) {
+    super(props)
+    this.state = {
+        showList:true
+    }
+
+}
+
+
+
+
+  render() {
+      const {showList} = this.state
+      const{users}=this.props
+
+
+
+
+          const usersItems = users && users.map(user => {
+              return (
+                  <li className="list-group-item">
+                      <span>{user.name}</span>
+                  </li>
+              )
+          })
+          return (
+              <div className="container">
+                  <h2>Lista de usuarios</h2>
+                  <button onClick= {()=>this.setState( {showList: !this.state.showList})}>
+                      showList
+                  </button>
+                  { showList &&
+
+                      <ul className="list-group user-list">
+                          {usersItems}
+                      </ul>
+
+                  }
+
+              </div>
+          )
+      }
+
+
+
+
+}
+
 
 export default UserList
