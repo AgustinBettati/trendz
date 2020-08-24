20019f8d8964506480f611d226275f54a6d494b7
agusdelavega <delavega.agus@gmail.com>
2020-08-24 00:22:45 -0300

    ISSUE-0115 user creation tests

--- server/src/main/java/facultad/trendz/dto/UserResponseDTO.java
+++ server/src/main/java/facultad/trendz/dto/UserResponseDTO.java
@@ -3,14 +3,27 @@ package facultad.trendz.dto;
 import facultad.trendz.model.Role;
 
 public class UserResponseDTO {
+
+    private Long id;
     private String email;
     private String username;
     private Role role;
 
-    public UserResponseDTO(String email, String username, Role role) {
+    public UserResponseDTO(Long id, String email, String username, Role role) {
+        this.id = id;
         this.email = email;
         this.username = username;
         this.role = role;
+    }
+
+    public UserResponseDTO(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getEmail() {
