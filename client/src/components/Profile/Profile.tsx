98d73fe0dae07a48ebad53a99c588d3a191196e7
sofiasidanez <sofia.sidanez@ing.austral.edu.ar>
2020-08-24 18:49:14 -0300

    ISSUE-0116: improved code

--- client/src/components/Profile/Profile.tsx
+++ client/src/components/Profile/Profile.tsx
@@ -1,8 +1,6 @@
 import React, {Component} from 'react'
 import {User} from '../types/types'
-import {TrendzButton} from "../common/TrendzButton/TrendzButton";
 import {TrendzInput} from "../common/TrendzInput/TrendzInput";
-import {Link, Redirect} from 'react-router-dom';
 import './Profile.css';
 
 
@@ -26,9 +24,7 @@ class Profile extends Component<Props> {
                     <div className={'title'}>
                         Profile
                     </div>
-
                     <TrendzInput value={"Sofia"} disabled={true} label={"Username"}/>
-
                     <TrendzInput value={"sofiasdz@ing.austral.edu.ar"} disabled={true} label={"Email"}/>
                     <div className={'button-c'}>
                         <a href="http://localhost:3000/editProfile" className="button">Edit</a>
