d48acafa8c379b1c2b897e8423054350ebdefc22
sofiasidanez <sofia.sidanez@ing.austral.edu.ar>
2020-08-25 18:54:31 -0300

    ISSUE-0128:  finished card component

--- client/src/components/common/TrendzCard/TrendzCard.tsx
+++ client/src/components/common/TrendzCard/TrendzCard.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import './TrendzInput.css';
+import './TrendzCard.css';
 import logo from "../../../assets/TrendzLogo.png";
 
 type CardProps = {
     title?: string,
     height: number,
+    children: React.ReactNode,
 }
 
 export const TrendzCard = (props: CardProps) => (
@@ -13,28 +14,12 @@ export const TrendzCard = (props: CardProps) => (
             <div className={'header'}>
                 <img className={'trendz-logo'} src={logo} alt={''}/>
                 <div className={'divisor'}/>
-                <div className={'title'}> props.title</div>
-                <div className={'body'}>
-                    height={props.height}
+                <div className={'title'}>{props.title}</div>
+                <div className={'body'} style={{"height" : props.height+"px"}}>
+                    {props.children}
                 </div>
             </div>
         </div>
     </div>
 
-
-
-
-
-    /* <div className={'input-wrapper'}>
-         <div className={'input-label'}>{props.label}</div>
-         <input
-             onChange={props.onChange}
-             disabled={props.disabled}
-             placeholder={props.placeholder}
-             type={props.password ? 'password' : 'text'}
-             value={props.value}
-             onFocus={props.onFocus}
-             onBlur={props.onBlur}
-         />
-     </div>*/
 )
