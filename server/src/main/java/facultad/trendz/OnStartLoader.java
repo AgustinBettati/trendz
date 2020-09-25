0a4c0cb72a7d6f8eb212013efc4bc96ce0a70bbc
sofiasidanez <sofia.sidanez@ing.austral.edu.ar>
2020-09-25 18:44:35 -0300

    added some mock topics and posts

--- server/src/main/java/facultad/trendz/OnStartLoader.java
+++ server/src/main/java/facultad/trendz/OnStartLoader.java
@@ -45,9 +45,9 @@ class OnStartLoader implements ApplicationRunner {
         userRepository.save(new User("1@gmail.com", "1", passwordEncoder.encode("1"), role));
         userRepository.save(new User("2@gmail.com", "2", passwordEncoder.encode("2"), role));
         userRepository.save(new User("3@gmail.com", "3", passwordEncoder.encode("3"), role));
-        topicRepository.save(new Topic("Movies1","Here we talk about recent theatrical releases", new Date()));
-        topicRepository.save(new Topic("Movies2","Here we talk about recent theatrical releases", new Date()));
-        topicRepository.save(new Topic("Movies3","Here we talk about recent theatrical releases", new Date()));
+        topicRepository.save(new Topic("Movies","Here we talk about recent theatrical releases", new Date()));
+        topicRepository.save(new Topic("Recipes","Here we share our favourite recipes :)", new Date()));
+        topicRepository.save(new Topic("Books","Post about your new favourite book here!", new Date()));
         topicRepository.save(new Topic("Movies4","Here we talk about recent theatrical releases", new Date()));
         topicRepository.save(new Topic("Movies5","Here we talk about recent theatrical releases", new Date()));
         topicRepository.save(new Topic("Movies6","Here we talk about recent theatrical releases", new Date()));
@@ -58,6 +58,18 @@ class OnStartLoader implements ApplicationRunner {
         topicRepository.save(new Topic("Movies11","Here we talk about recent theatrical releases", new Date()));
         topicRepository.save(new Topic("Movies12","Here we talk about recent theatrical releases", new Date()));
         postRepository.save(new Post("Star Wars","Han Solo was the best","alink",new Date(),topicRepository.getTopicById(Long.valueOf(1))));
+        postRepository.save(new Post("Star Trek","Loved This Movie","https://es.wikipedia.org/wiki/Star_Trek",new Date(),topicRepository.getTopicById(Long.valueOf(1))));
+        postRepository.save(new Post("Twilight","My daughter loved the werewolfs","alink",new Date(),topicRepository.getTopicById(Long.valueOf(1))));
+        postRepository.save(new Post("Casablanca","The plane scene made me cry","https://es.wikipedia.org/wiki/Star_Trek",new Date(),topicRepository.getTopicById(Long.valueOf(1))));
 
+        postRepository.save(new Post("Steamed Potatoes","For this recipe you'll need potatoes, salt and pepper.","alink",new Date(),topicRepository.getTopicById(Long.valueOf(2))));
+        postRepository.save(new Post("Ham and Spagetti Sandwich","Unusual, but actually very tasty","https://es.wikipedia.org/wiki/Star_Trek",new Date(),topicRepository.getTopicById(Long.valueOf(2))));
+        postRepository.save(new Post("Bananna Cupacake","Ingredients: Bananna, Flour, Milk, Eggs and Sugar","alink",new Date(),topicRepository.getTopicById(Long.valueOf(2))));
+        postRepository.save(new Post("Chocolate-chip Brownies","You'll need chocolate, flour,milk and lots of love!","https://es.wikipedia.org/wiki/Star_Trek",new Date(),topicRepository.getTopicById(Long.valueOf(2))));
+
+        postRepository.save(new Post("The Martian Chronicles, by Ray Braudbury","I love this books intriging take on the future","alink",new Date(),topicRepository.getTopicById(Long.valueOf(3))));
+        postRepository.save(new Post("Turning Point, BY Hayao Miyasaki","This book give great insight on the worlds greatest animator","https://es.wikipedia.org/wiki/Star_Trek",new Date(),topicRepository.getTopicById(Long.valueOf(3))));
+        postRepository.save(new Post("Foundation, by Issac Asimov","A bit confusing but i read it till the end!","alink",new Date(),topicRepository.getTopicById(Long.valueOf(3))));
+        postRepository.save(new Post("The Strange Case of Dr Jekyll and Mr Hyde","I just love curling up with a hot cup of tea and this book in my hand!","https://es.wikipedia.org/wiki/Star_Trek",new Date(),topicRepository.getTopicById(Long.valueOf(3))));
     }
 }
