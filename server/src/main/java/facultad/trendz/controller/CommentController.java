b296682335f37453e82fbde86ff011597d3c7a9e
agusdelavega <delavega.agus@gmail.com>
2020-10-06 22:15:43 -0300

    ISSUE-0198 endpoint edit comment

--- server/src/main/java/facultad/trendz/controller/CommentController.java
+++ server/src/main/java/facultad/trendz/controller/CommentController.java
@@ -9,13 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.stream.Collectors;
@@ -32,17 +30,30 @@ public class CommentController {
     }
 
     @PostMapping("/post/{postId}/comment")
-    public ResponseEntity<Object> createPost(@Valid @RequestBody CommentCreateDTO comment, BindingResult bindingResult, @PathVariable Long postId, Authentication authentication) {
-        if (bindingResult.hasErrors()) {
-            final HttpStatus status = HttpStatus.BAD_REQUEST;
-            String error = bindingResult.getAllErrors().stream().map(DefaultMessageSourceResolvable::getDefaultMessage).collect(Collectors.joining(", "));
-            MessageResponseDTO body = new MessageResponseDTO(error);
-            return new ResponseEntity<>(body, status);
-        }
+    public ResponseEntity<Object> createComment(@Valid @RequestBody CommentCreateDTO comment, BindingResult bindingResult, @PathVariable Long postId, Authentication authentication) {
+        if (bindingResult.hasErrors()) return getInvalidDTOResponse(bindingResult);
+
         MyUserDetails userDetails = (MyUserDetails) authentication.getPrincipal();
 
         final CommentResponseDTO body = commentService.saveComment(comment, postId, userDetails.getId());
         final HttpStatus status = HttpStatus.CREATED;
+        return new ResponseEntity<>(body, status);
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN') or @commentService.commentAuthorVerification(#commentId,#authentication)")
+    @PutMapping("/comment/{commentId}")
+    public ResponseEntity<Object> editComment(@Valid @RequestBody CommentCreateDTO comment, BindingResult bindingResult, @PathVariable Long commentId, Authentication authentication) {
+        if (bindingResult.hasErrors()) return getInvalidDTOResponse(bindingResult);
+
+        final CommentResponseDTO body = commentService.editComment(commentId, comment);
+        final HttpStatus status = HttpStatus.OK;
+        return new ResponseEntity<>(body, status);
+    }
+
+    private ResponseEntity<Object> getInvalidDTOResponse(BindingResult bindingResult) {
+        final HttpStatus status = HttpStatus.BAD_REQUEST;
+        String error = bindingResult.getAllErrors().stream().map(DefaultMessageSourceResolvable::getDefaultMessage).collect(Collectors.joining(", "));
+        MessageResponseDTO body = new MessageResponseDTO(error);
         return new ResponseEntity<>(body, status);
     }
 
