a2d1ab3d423c5b8f875d449b173db6f9abacd6b0
Facundo Rivas <facundo.rivas@ing.austral.edu.ar>
2020-10-24 22:17:13 -0300

    Display search results wip

--- client/src/components/common/AppHeader/AppHeader.tsx
+++ client/src/components/common/AppHeader/AppHeader.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {NavLink, withRouter} from 'react-router-dom';
 import './AppHeader.css';
 import logo from '../../../assets/TrendzLogo.png';
@@ -6,9 +6,15 @@ import {MdSearch} from 'react-icons/md';
 
 const AppHeader = ({history}: any) => {
 
+    const [toSearch, setToSearch] = useState('')
+
     const handleLogout = () => {
         localStorage.removeItem('token');
         history.push('/');
+    }
+
+    const handleSearch = () => {
+        toSearch && history.push('/main/search/' + toSearch);
     }
 
     return (
@@ -22,8 +28,14 @@ const AppHeader = ({history}: any) => {
                     </div>
                     <div className={"searchbar-container"}>
                         <div className={"searchbar"}>
-                            <MdSearch size={20} color={'white'}/>
-                            <input placeholder={"Search...."} style={{width: '500px', height: "30%",}}></input>
+                            <input
+                                placeholder={"Search...."}
+                                onChange={(e) => setToSearch(e.target.value)}
+                                onKeyDown={(e) => {if (e.key === 'Enter') handleSearch()}}
+                            />
+                            <div className={toSearch ? 'searchbar-button-enabled' : 'searchbar-button-disabled'} onClick={() => handleSearch()}>
+                                <MdSearch size={20} color={'white'}/>
+                            </div>
                         </div>
                     </div>
                     <NavLink to={"/main/profile"} className={"profile"}>Profile</NavLink>
