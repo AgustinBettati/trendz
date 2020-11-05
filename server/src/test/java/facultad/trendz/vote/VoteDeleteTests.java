351535f559a2f509b3ec5ab2b58fd170cc248950
sofiasidanez <sofia.sidanez@ing.austral.edu.ar>
2020-11-05 12:25:56 -0300

    ISSUE-0190: Implemented Tests delete vote

--- server/src/test/java/facultad/trendz/vote/VoteDeleteTests.java
+++ server/src/test/java/facultad/trendz/vote/VoteDeleteTests.java
@@ -2,6 +2,7 @@
 package facultad.trendz.vote;
 
 import facultad.trendz.TestUtils;
+import facultad.trendz.dto.MessageResponseDTO;
 import facultad.trendz.dto.post.PostCreateDTO;
 import facultad.trendz.dto.post.PostResponseDTO;
 import facultad.trendz.dto.vote.VoteResponseDTO;
@@ -45,7 +46,7 @@ public class VoteDeleteTests  extends TestUtils {
 
 
     @Test
-    public void testDeletePostWhenPostIsDownVoted() throws URISyntaxException {
+    public void testDeleteVoteWhenPostIsDownVoted() throws URISyntaxException {
 
         RestTemplate restTemplate = new RestTemplate();
 
@@ -56,10 +57,10 @@ public class VoteDeleteTests  extends TestUtils {
 
 
         //post new Topic
-        ResponseEntity<TopicResponseDTO> topicResponse = postTopic(jwtToken, "testTopicUpvotedPost1", "test description", randomServerPort);
+        ResponseEntity<TopicResponseDTO> topicResponse = postTopic(jwtToken, "testTopicDeleteDownvote", "test description", randomServerPort);
 
         //post new Post
-        ResponseEntity<PostResponseDTO> postResponse = postPost(jwtToken, "testTitleUpvotedPost1", "test description", "testLink.com", topicResponse.getBody().getId(), randomServerPort);
+        ResponseEntity<PostResponseDTO> postResponse = postPost(jwtToken, "testTitleDeleteDownvote", "test description", "testLink.com", topicResponse.getBody().getId(), randomServerPort);
 
 
         downvotePost(postResponse.getBody().getId(),jwtToken,randomServerPort);
@@ -69,29 +70,64 @@ public class VoteDeleteTests  extends TestUtils {
 
 
         HttpEntity entity = new HttpEntity<>(headers);
-        final String url = "http://localhost:" + randomServerPort + "/vote/"+postResponse.getBody().getId()+"/upvote";
+        final String url = "http://localhost:" + randomServerPort + "/vote/"+postResponse.getBody().getId();
         URI uri = new URI(url);
 
 
-        //WHEN MAKING AN UPVOTE IN A POST
-        ResponseEntity<VoteResponseDTO> response = restTemplate.exchange(uri, HttpMethod.POST, entity, VoteResponseDTO.class);
+        //WHEN DELETING A DOWNVOTE
+        ResponseEntity<MessageResponseDTO> response = restTemplate.exchange(uri, HttpMethod.DELETE, entity,MessageResponseDTO.class);
         //THEN
 
 
-        Optional<User> user= userRepository.findById(response.getBody().getUserId()); // Post also updated on db
-        Assert.assertTrue(user.isPresent());
-        Assert.assertEquals(201, response.getStatusCodeValue());
+        Assert.assertEquals(200, response.getStatusCodeValue());
 
 
         //verifying downvote has been deleted
 
         Assert.assertFalse(this.voteRepository.findByPostIdAndUserIdAndIsUpvote(postResponse.getBody().getUserId(),postResponse.getBody().getId(),!true).isPresent());
 
-        Assert.assertTrue(this.voteRepository.findByPostIdAndUserIdAndIsUpvote(postResponse.getBody().getUserId(),postResponse.getBody().getId(),true).isPresent());
-        Assert.assertEquals(user.get().getEmail(),"admin@gmail.com");
+    }
+
+    @Test
+    public void testDeleteVoteWhenPostIsUpVoted() throws URISyntaxException {
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        //lOGIN WITH ADMIN
+        ResponseEntity<JwtResponseDTO> loginResponse = loginUser("admin@gmail.com", "admin", randomServerPort);
+
+        String jwtToken = loginResponse.getBody().getToken();
 
 
+        //post new Topic
+        ResponseEntity<TopicResponseDTO> topicResponse = postTopic(jwtToken, "testTopicDeleteUpvote", "test description", randomServerPort);
 
+        //post new Post
+        ResponseEntity<PostResponseDTO> postResponse = postPost(jwtToken, "testTitleDeleteUpvote", "test description", "testLink.com", topicResponse.getBody().getId(), randomServerPort);
+
+
+        upvotePost(postResponse.getBody().getId(),jwtToken,randomServerPort);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Bearer " + jwtToken);
+
+
+        HttpEntity entity = new HttpEntity<>(headers);
+        final String url = "http://localhost:" + randomServerPort + "/vote/"+postResponse.getBody().getId();
+        URI uri = new URI(url);
+
+
+        //WHEN DELETING AN UPVOTE
+        ResponseEntity<MessageResponseDTO> response = restTemplate.exchange(uri, HttpMethod.DELETE, entity,MessageResponseDTO.class);
+        //THEN
+
+
+        Assert.assertEquals(200, response.getStatusCodeValue());
+
+
+        //verifying upvote has been deleted
+
+        Assert.assertFalse(this.voteRepository.findByPostIdAndUserIdAndIsUpvote(postResponse.getBody().getUserId(),postResponse.getBody().getId(),true).isPresent());
 
     }
 
